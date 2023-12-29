72e5831be73a38437ea632707fee03f1e7f652cc
gregrylivingston <livingsgb@gmail.com>
2023-12-29 12:12:56 -0500

    Better gotolocation button

--- scene/button/button_go_to_location.gd
+++ scene/button/button_go_to_location.gd
@@ -1,6 +1,8 @@
 extends Button
 
 @export var newSceneName: String
+var focused = false
+@onready var radialProgressBar = $RadialProgress
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,8 +11,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if focused && radialProgressBar.progress < 30 : 
+		radialProgressBar.progress += 2
 
 
 func _on_pressed():
 	get_tree().change_scene_to_file("res://scene/location/" + newSceneName + ".tscn")
+
+
+func _on_mouse_entered():
+	focused = true
+
+func _on_mouse_exited():
+	focused = false
+	radialProgressBar.progress = 0
