3647ca703a7a68a456e6b4333085fe894e0b0dab
gregrylivingston <livingsgb@gmail.com>
2023-12-29 21:04:44 -0500

    Map basics

--- scene/button/button_go_to_location.gd
+++ scene/button/button_go_to_location.gd
@@ -1,12 +1,22 @@
+@tool
 extends Button
 
 @export var newSceneName: String
+@export var isTiny: bool = false
+
 var focused = false
+
 @onready var radialProgressBar = $RadialProgress
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tooltip_text = "Walk to " + newSceneName.replace("_"," ")
+	if isTiny:
+		custom_minimum_size = Vector2i(20,20)
+		size = custom_minimum_size
+		$RadialProgress.position = custom_minimum_size/2
+		$RadialProgress.radius = 12
+		add_theme_stylebox_override("normal",load("res://scene/location/locationButtonStyleBoxTiny.tres"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,11 +29,17 @@ func _on_pressed():
 	$AudioStreamPlayer2.play()
 	Game.gameroot.goToPlace(newSceneName)
 	
+	#close map if open
+	if Game.gameroot.map != null:
+		Game.gameroot.map.queue_free()
+	
 
 
 func _on_mouse_entered():
 	focused = true
-	$AnimationPlayer.play("mouseOver")
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "size", size * 1.1, .1)
+	tween.tween_property(self, "size", size / 1.1, .1)
 	$AudioStreamPlayer.play()
 
 func _on_mouse_exited():
--- scene/gameroot.gd
+++ scene/gameroot.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @onready var currentPlace = $PlaceParent/Place
+var map = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,3 +27,9 @@ func _on_option_menu_b_pressed():
 	var newScene = load("res://scene/ui/menu_options.tscn").instantiate()
 	newScene.ingameSetup()
 	add_child(newScene)
+
+
+func _on_map_button_2_pressed():
+	var newScene = load("res://scene/ui/Map.tscn").instantiate()
+	add_child(newScene)
+	map = newScene
