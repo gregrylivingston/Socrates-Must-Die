1b65ba831d7f82043d94b5656113b1c80c6ea7b5
gregrylivingston <livingsgb@gmail.com>
2023-12-31 18:25:09 -0500

    trechus v2

--- script/global/Juror.gd
+++ script/global/Juror.gd
@@ -4,19 +4,30 @@ extends Node
 @export var list = [
 	{
 		"name":"Trechus",
-		"accuser":false,
 		"image":"res://character/Trechus/Trechus1.png",
+		"achieved":false,
+		"hasConvinced":false,
 	},
 	{
 		"name":"Olympiodoros",
 		"image":"res://character/Olympiodoros/Olympiodoros1.png",
+		"achieved":false,
+		"hasConvinced":false,
 	},
 	{
 		"name":"Menander",
 		"image":"res://character/Menander/Menander1.png",
+		"achieved":false,
+		"hasConvinced":false,
 	},
 	
 ]
+
+func convinceJuror(jurorName):
+	var myJuror = list.find("name" == jurorName)
+	myJuror.achieved = true
+	myJuror.hasConvinced = true
+	
 
 var temples = ["zeus","parthanon","Haphaestus","Anakeion"]
 
